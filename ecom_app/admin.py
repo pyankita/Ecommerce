64da23a708c0ac1de6cf18124f8cfa1e6a99944d
pyankita <pyankitaghartimagar@gmail.com>
2025-05-17 20:43:24 +0545

    landing page

--- ecom_app/admin.py
+++ ecom_app/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 from .models import (
     Category, Product, Customer, CartItem,
-    Order, OrderItem, Review, Wishlist
+    Order, OrderItem, Review
 )
 
 admin.site.register(Category)
@@ -13,4 +13,4 @@ admin.site.register(CartItem)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 admin.site.register(Review)
-admin.site.register(Wishlist)
+
--- ecom_app/models.py
+++ ecom_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
